8e72521fe4749f1f8610f07e84884d231017ed47
Yash Saxena <yash.saxena@gmail.com>
2025-01-10 16:04:04 -0600

    Hungry boi

--- main.py
+++ main.py
@@ -39,40 +39,58 @@ def end(game_state: typing.Dict):
     print("GAME OVER\n")
 
 
+def manhattan_distance(pt1, pt2):
+    return abs(pt1["x"] - pt2["x"]) + abs(pt1["y"] - pt2["y"])
+
 # move is called on every turn and returns your next move
 # Valid moves are "up", "down", "left", or "right"
 # See https://docs.battlesnake.com/api/example-move for available data
 def move(game_state: typing.Dict) -> typing.Dict:
+    print(game_state)
 
+    move_updates = {"up" : (0, 1), "down": (0, -1), "right": (1, 0), "left": (-1, 0)}
     is_move_safe = {"up": True, "down": True, "left": True, "right": True}
 
     # We've included code to prevent your Battlesnake from moving backwards
+    # Note: (0, 0) is bottom left
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
     my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
 
     if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
         is_move_safe["left"] = False
-
     elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
         is_move_safe["right"] = False
-
     elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
         is_move_safe["down"] = False
-
     elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
         is_move_safe["up"] = False
 
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
+    board_width = game_state['board']['width']
+    board_height = game_state['board']['height']        
 
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
+    if my_head["x"] == 0:
+        is_move_safe["left"] = False
+    elif my_head["x"] == board_width - 1:
+        is_move_safe["right"] = False
+    
+    if my_head["y"] == 0:
+        is_move_safe["down"] = False
+    elif my_head["y"] == board_height - 1:
+        is_move_safe["up"] = False
 
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
+    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself or others
+    snakes = game_state["board"]["snakes"]
+    move_cell = lambda cell, move: {"x" : cell["x"] + move_updates[move][0], "y" : cell["y"] + move_updates[move][1]}
+    move_cells = {move : move_cell(my_head, move) for move in {"up", "down", "left", "right"}}
+    for snake in snakes:
+        body_cells = snake["body"]
+        for move, move_cell in move_cells.items():
+            if move_cell in body_cells:
+                is_move_safe[move] = False
 
     # Are there any safe moves left?
+    print(is_move_safe)
     safe_moves = []
     for move, isSafe in is_move_safe.items():
         if isSafe:
@@ -86,8 +104,17 @@ def move(game_state: typing.Dict) -> typing.Dict:
     next_move = random.choice(safe_moves)
 
     # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    # food = game_state['board']['food']
-
+    food_locations = game_state['board']['food']
+    if len(food_locations) > 0:
+        smallest_distance, closest_apple = 1e99, {}
+        for apple in food_locations:
+            distance = manhattan_distance(apple, my_head)
+            if distance <= smallest_distance:
+                smallest_distance = distance
+                closest_apple = apple
+        
+        next_move = min(safe_moves, key=lambda move: manhattan_distance(closest_apple, move_cells[move]))
+    
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
 
